05fb3e9dd5e35d4f2c9c49978f19bea61a13cfc5
Emmanuel Odongo <odiwourmanu123@gmail.com>
2026-06-25 08:47:01 +0300

    Fix: Google OAuth

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,15 +1,32 @@
 # core/serializers.py
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from allauth.account.utils import setup_user_email
 
 class CustomRegisterSerializer(RegisterSerializer):
+    # Hide the username field from the API payload validation
     username = None
-
-    def validate_username(self, value):
-        return value
 
     def get_cleaned_data(self):
         return {
             'email': self.validated_data.get('email', ''),
-            'password1': self.validated_data.get('password1', ''),
-            'password2': self.validated_data.get('password2', ''),
         }
+
+    def save(self, request):
+        # 1. Instantiate the default user instance
+        user = super().save(request)
+
+        # 2. Extract the prefix of the email to populate Django's required username field
+        email = self.cleaned_data.get('email')
+        base_username = email.split('@')[0]
+
+        # 3. Handle duplicates by appending a suffix if the username already exists
+        User = user.__class__
+        username = base_username
+        num = 1
+        while User.objects.filter(username=username).exists():
+            username = f"{base_username}_{num}"
+            num += 1
+
+        user.username = username
+        user.save()
+        return user
